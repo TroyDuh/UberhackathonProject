e451aef8014d87a1159c974ffd88b2989611c96b
pliantart525233 <67117443+pliantart525233@users.noreply.github.com>
2023-09-15 18:13:29 -0500

    Header

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+/*
+* Group Members: João Vítor Chaves Avila, Troy Anderson The, Dimas Deffieux
+* Project created on September 15th 2023
+* Main file that will be what the user will run
+* */
+
 public class Main {
     public static void main(String[] args)
     {
